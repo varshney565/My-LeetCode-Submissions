9b28437a7cafd99165aa3ebcada777f0f216f423
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-08-08 17:50:41 +0530

    Time: 500 ms (26.88%), Space: 109.7 MB (24.18%) - LeetHub

---
+++ 839-similar-string-groups/839-similar-string-groups.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    class dsu{
+        public:
+        vector<int> p;
+        dsu(int n){
+            p.resize(n);
+            iota(p.begin(),p.end(),0);
+        }
+        int get(int n){
+            if(p[n] == n){
+                return n;
+            }
+            return p[n] = get(p[n]);
+        }
+        bool unite(int a,int b){
+            int p1 = get(a);
+            int p2 = get(b);
+            if(p1 == p2){
+                return false;
+            }
+            p[p1] = p2;
+            return true;
+        }
+    };
+    bool isSimilar(string a,string b){
+        int cnt = 0;
+        int n = a.size();
+        for(int i = 0 ; i < n ; i++){
+            if(a[i] != b[i])
+                cnt++;
+        }
+        return (cnt<=2);
+    }
+    
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        int compo = n;
+        dsu D(n);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                if(isSimilar(strs[i],strs[j])){
+                    if(D.unite(i,j))
+                        compo--;
+                }
+            }
+        }
+        return compo;
+    }
+};
