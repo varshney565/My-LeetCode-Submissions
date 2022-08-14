1553cca4aa8c324cbe1c84e192589af5a6bafdc6
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-08-14 17:36:29 +0530

    Time: 120 ms (7.61%), Space: 14 MB (91.66%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -5,12 +5,17 @@ public:
         ans.push_back(source);
         if(ans.size() == total+1)
             return true;
+        if(graph.find(source) == graph.end()){
+            ans.pop_back();
+            return false;
+        }
         for(int i = 0 ; i < graph[source].size() ; i++){
             string child = graph[source][i];
             graph[source].erase(graph[source].begin()+i);
             if(dfs(child,ans,graph,total))
                 return true;
             graph[source].insert(graph[source].begin()+i,child);
+            // graph[source].push_back(child);
         }
         ans.pop_back();
         return false;
@@ -23,7 +28,12 @@ public:
         }
         int n = tickets.size();
         for(auto &[a,x] : graph){
+            cout<<a<<" : ";
             sort(x.begin(),x.end());
+            for(auto t : x){
+                cout<<t<<" ";
+            }
+            cout<<"\n";
         }
         vector<string> ans;
         dfs("JFK",ans,graph,n);
