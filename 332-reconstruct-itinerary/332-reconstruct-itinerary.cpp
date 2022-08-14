b5866ecea454c0f439d0946739420c37176b381c
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-08-14 22:07:54 +0530

    Time: 26 ms (83.62%), Space: 14.2 MB (76.41%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    
-    bool dfs(string source,vector<string> &ans , map<string,vector<string>> &graph,int total){
+    void dfs(string source,vector<string> &ans,map<string,vector<string>> &graph){
+        while(graph[source].size() > 0){
+            string child = graph[source].back();
+            graph[source].pop_back();
+            dfs(child,ans,graph);
+        }
         ans.push_back(source);
-        if(ans.size() == total+1)
-            return true;
-        if(graph.find(source) == graph.end()){
-            ans.pop_back();
-            return false;
-        }
-        for(int i = 0 ; i < graph[source].size() ; i++){
-            string child = graph[source][i];
-            graph[source].erase(graph[source].begin()+i);
-            if(dfs(child,ans,graph,total))
-                return true;
-            graph[source].insert(graph[source].begin()+i,child);
-            // graph[source].push_back(child);
-        }
-        ans.pop_back();
-        return false;
     }
     
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         map<string,vector<string>> graph;
-        for(int i = 0 ; i < tickets.size() ; i++){
+        int n = tickets.size();
+        for(int i = 0 ; i < n ; i++){
             graph[tickets[i][0]].push_back(tickets[i][1]);
         }
-        int n = tickets.size();
         for(auto &[a,x] : graph){
-            cout<<a<<" : ";
-            sort(x.begin(),x.end());
-            for(auto t : x){
-                cout<<t<<" ";
-            }
-            cout<<"\n";
+            sort(x.begin(),x.end(),greater<string>());
         }
         vector<string> ans;
-        dfs("JFK",ans,graph,n);
+        dfs("JFK",ans,graph);
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
