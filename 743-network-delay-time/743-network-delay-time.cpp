b6e3f4b33399f2185519494ca561be4f0ef4f805
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-08-24 23:44:02 +0530

    Time: 183 ms (62.49%), Space: 41 MB (53.81%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -9,31 +9,31 @@ public:
         }
         k--;
         set<vector<int>> PendingNodes;
-        PendingNodes.insert({0,k,-1,0});
+        PendingNodes.insert({0,k});
+        vector<int> dis(n,INT_MAX);
         vector<int> visited(n,false);
-        int ans = 0;
+        vector<int> parent(n,-1);
+        dis[k] = 0;
         while(!PendingNodes.empty()){
             auto front = *PendingNodes.begin();
             PendingNodes.erase(front);
             int wsf = front[0];
-            int u = front[1];
-            int p = front[2];
-            int w = front[3];
-            if(visited[u]){
+            int src = front[1];
+            if(visited[src])
                 continue;
-            }
-            visited[u] = true;
-            ans = max(ans,wsf);
-            for(int i = 0 ; i < graph[u].size() ; i++){
-                pair<int,int> v = graph[u][i];
-                if(!visited[v.first]){
-                    PendingNodes.insert({wsf+v.second,v.first,u,v.second});
+            visited[src] = true;
+            for(auto child : graph[src]){
+                if(!visited[child.first] && dis[src]+child.second < dis[child.first]){
+                    dis[child.first] = dis[src]+child.second;
+                    PendingNodes.insert({dis[child.first],child.first});
+                    parent[child.first] = src;
                 }
             }
         }
+        int ans = INT_MIN;
         for(int i = 0 ; i < n ; i++){
-            if(!visited[i])
-                return -1;
+            if(!visited[i]) return -1;
+            ans = max(ans,dis[i]);
         }
         return ans;
     }
