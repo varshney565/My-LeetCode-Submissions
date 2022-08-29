f5d8319e1b423e6a24a810fce46fc829f2eb1e38
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-08-30 00:23:54 +0530

    Time: 93 ms (33.61%), Space: 52.1 MB (11.88%) - LeetHub

--- 639-decode-ways-ii/639-decode-ways-ii.cpp
+++ 639-decode-ways-ii/639-decode-ways-ii.cpp
@@ -12,30 +12,20 @@ public:
             if(dp[st] != -1) return dp[st];
             long long ans = 0;
             if(s[st] == '*'){
-                //taking one
-                //* | anything
-                //* ans += 9*ans(st+1)
                 ans += (9LL*go(st+1))%mod;
-                
                 if(st+1 < n){
                     int r = go(st+2);
                     if(s[st+1] != '*'){
-                        //taking two 
-                        //* num
-                        //* --> num <= 6  ans += 2*ans(st+2)
-                        //      num >  6  ans += ans(st+2)
                         if(s[st+1] <= '6'){
                             ans += (2LL*r)%mod;
                         }else{
                             ans += r;
                         }
                     }else{
-                        //* * posibilities = 17
                         ans += (15LL*r)%mod;
                     }
                 }
             }else{
-                //num | anything
                 ans += go(st+1);
                 if(st+1 < n){
                     int r = go(st+2);
@@ -48,7 +38,6 @@ public:
                             ans += (6LL*r)%mod;
                         }
                     }else{
-                        //num num
                         int num = (s[st]-'0')*10LL+(s[st+1]-'0');
                         if(num <= 26LL)
                             ans += r;
