6287dfafd333cd2f22b452671d9c0f776678bb6d
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-03 18:06:44 +0530

    Time: 69 ms (39.91%), Space: 30.2 MB (17.14%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,-1));
-        function<int(int,int)> go = [&](int n,int m){
-            if(m == 0)
-                return dp[n][m] = 1;
-            if(n == 0)
-                return dp[n][m] = 0;
-            if(dp[n][m] != -1) return dp[n][m];
-            int ans = 0;
-            if(s[n-1] == t[m-1]){
-                ans += go(n-1,m-1);
+        vector<vector<long long>> dp(s.size()+1,vector<long long>(t.size()+1,-1));
+        const long long mod = INT_MAX+1;
+        function<int(int,int)> go = [&](int N,int M){
+            for(int n = 0 ; n <= N ; n++){
+                for(int m = 0 ; m <= M ; m++){
+                    if(m == 0){
+                        dp[n][m] = 1LL;continue;
+                    }
+                    if(n == 0){
+                        dp[n][m] = 0LL;continue;
+                    }
+                    
+                    long long ans = 0;
+                    if(s[n-1] == t[m-1]){
+                        ans = ((long long)(dp[n-1][m-1]+dp[n-1][m]))%mod;
+                    }else{
+                        ans = dp[n-1][m];
+                    }
+                    dp[n][m] = ans;
+                }
             }
-            ans += go(n-1,m);
-            return dp[n][m] = ans;
+            return dp[N][M];
         };
         return go(s.size(),t.size());
     }
