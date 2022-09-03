d10811124515d159cd99111ad01190966074f8ef
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-03 17:34:45 +0530

    Time: 23 ms (49.68%), Space: 9 MB (59.27%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,35 +4,39 @@ public:
         int n = word1.size();
         int m = word2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        function<int(int,int)> go = [&](int n,int m){
-            if(n == 0 || m == 0){
-                if(n){
-                    return dp[n][m] = n;
-                }else if(m){
-                    return dp[n][m] = m;
-                }else{
-                    return dp[n][m] = 0;
+        function<int(int,int)> go = [&](int N,int M){
+            for(int n = 0 ; n <= N ; n++){
+                for(int m = 0 ; m <= M ; m++){
+                    if(n == 0 || m == 0){
+                        if(n){
+                            dp[n][m] = n;
+                        }else if(m){
+                            dp[n][m] = m;
+                        }else{
+                            dp[n][m] = 0;
+                        }
+                        continue;
+                    }
+
+                    int ans = 0;
+                    if(word1[n-1] != word2[m-1]){
+                        //end characters are not same
+
+                        //remove
+                        int choice1 = dp[n-1][m];
+                        //insert
+                        int choice2 = dp[n][m-1];
+                        //replace 
+                        int choice3 = dp[n-1][m-1];
+                        ans = min(choice1,min(choice2,choice3))+1;
+                    }else{
+                        //end characters are same
+                        ans += dp[n-1][m-1];
+                    }
+                    dp[n][m] = ans;
                 }
             }
-
-            if(dp[n][m] != -1) return dp[n][m];
-            
-            int ans = 0;
-            if(word1[n-1] != word2[m-1]){
-                //end characters are not same
-                
-                //remove
-                int choice1 = go(n-1,m);
-                //insert
-                int choice2 = go(n,m-1);
-                //replace 
-                int choice3 = go(n-1,m-1);
-                ans = min(choice1,min(choice2,choice3))+1;
-            }else{
-                //end characters are same
-                ans += go(n-1,m-1);
-            }
-            return dp[n][m] = ans;
+            return dp[N][M];
         };
         return go(n,m);
     }
