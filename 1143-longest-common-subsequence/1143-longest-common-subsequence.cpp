270baa886ac57fa6a0b0347cf29d1d6dc7927fad
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-03 13:51:23 +0530

    Time: 74 ms (8.93%), Space: 13.6 MB (12.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,17 +4,18 @@ public:
         int n = text1.size();
         int m = text2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        function<int(int,int)> go = [&](int i1,int i2){
-            if(i1 == n || i2 == m){
-                return dp[i1][i2] = 0;
+        function<int(int,int)> go = [&](int n,int m){
+            if(n == 0 || m == 0){
+                return dp[n][m] = 0;
             }
-            if(dp[i1][i2] != -1) return dp[i1][i2];
-            if(text1[i1] == text2[i2]){
-                return dp[i1][i2] = 1+go(i1+1,i2+1);
+            if(dp[n][m] != -1)
+                return dp[n][m];
+            if(text1[n-1] == text2[m-1]){
+                return dp[n][m] = 1+go(n-1,m-1);
             }else{
-                return dp[i1][i2] = max(go(i1+1,i2),go(i1,i2+1));
+                return dp[n][m] = max(go(n-1,m),go(n,m-1));
             }
         };
-        return go(0,0);
+        return go(n,m);
     }
 };
