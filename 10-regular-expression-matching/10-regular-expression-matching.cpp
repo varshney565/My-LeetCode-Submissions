d8bcfd150306ff77e7d00b0a5adc0f67e331eec4
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-05 17:39:39 +0530

    Time: 7 ms (69.03%), Space: 7.4 MB (32.23%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -30,6 +30,47 @@ public:
                 return dp[n][m] = 0;
             }
         };
-        return go(n,m);
+        
+        function<int(int,int)> go_dp = [&](int N,int M){
+            for(int n = 0 ; n <= N ; n++){
+                for(int m = 0 ; m <= M ; m++){
+                    if(n == 0 && m == 0) {
+                        dp[n][m] = 1;continue;
+                    }
+                    if(m == 0){
+                        dp[n][m] = 0;continue;
+                    }
+                    if(n == 0){
+                        int i = m-1;
+                        int ans = 1;
+                        while(i >= 0){
+                            if(p[i] != '*'){
+                                ans = 0;
+                                break;
+                            }
+                            i -= 2;
+                        }
+                        dp[n][m] = ans;
+                        continue;
+                    }
+                    
+                    if(s[n-1] == p[m-1]) dp[n][m] = dp[n-1][m-1];
+                    else if(p[m-1] == '.') dp[n][m] = dp[n-1][m-1];
+                    else if(p[m-1] == '*'){
+                        int res = 0;
+                        res = res || dp[n][m-2];
+                        res = res || dp[n][m-1];
+                        res = res || ((p[m-2] == s[n-1] || p[m-2] == '.') && dp[n-1][m-1]);
+                        res = res || ((p[m-2] == s[n-1] || p[m-2] == '.') && dp[n-1][m]);
+                        dp[n][m] = res;
+                    }else{
+                        dp[n][m] = 0;
+                    }
+                }
+            }
+            return dp[N][M];
+        };
+        
+        return go_dp(n,m);
     }
 };
