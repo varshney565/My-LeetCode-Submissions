ba19dd6943581459160ef47c1c7462a2a1af1deb
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-06 11:11:00 +0530

    Time: 45 ms (18.58%), Space: 13.1 MB (29.84%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -5,16 +5,21 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        function<int(int,int)> go = [&](int n,int m){
-            if(n == 0 || m == 0)
-                return dp[n][m] = 0;
-            if(dp[n][m] != -1)
-                return dp[n][m];
-            if(nums1[n-1] == nums2[m-1]){
-                return dp[n][m] = 1+go(n-1,m-1);
-            }else{
-                return dp[n][m] = max(go(n-1,m),go(n,m-1));
+        function<int(int,int)> go = [&](int N,int M){
+            for(int n = 0 ; n <= N ; n++){
+                for(int m = 0 ; m <= M ; m++){
+                    if(n == 0 || m == 0){
+                        dp[n][m] = 0;
+                        continue;
+                    }
+                    if(nums1[n-1] == nums2[m-1]){
+                        dp[n][m] = 1+dp[n-1][m-1];
+                    }else{
+                        dp[n][m] = max(dp[n-1][m],dp[n][m-1]);
+                    }
+                }
             }
+            return dp[N][M];
         };
         return go(n,m);
     }
