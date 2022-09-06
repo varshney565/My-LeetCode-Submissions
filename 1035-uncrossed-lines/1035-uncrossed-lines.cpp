dd8a8fb31bd0d21b36780e6343bf6ba34059db48
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-06 11:07:27 +0530

    Time: 95 ms (7.01%), Space: 13.2 MB (6.22%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        //LCS Question ...
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        function<int(int,int)> go = [&](int n,int m){
+            if(n == 0 || m == 0)
+                return dp[n][m] = 0;
+            if(dp[n][m] != -1)
+                return dp[n][m];
+            if(nums1[n-1] == nums2[m-1]){
+                return dp[n][m] = 1+go(n-1,m-1);
+            }else{
+                int ans = 0;
+                ans = max(ans,go(n-1,m));
+                ans = max(ans,go(n,m-1));
+                return dp[n][m] = ans;
+            }
+        };
+        return go(n,m);
+    }
+};
