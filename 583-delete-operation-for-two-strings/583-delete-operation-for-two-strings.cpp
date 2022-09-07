c9829f5118b114231cb7d055ccd80c10437ba255
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-07 15:51:28 +0530

    Time: 67 ms (17.18%), Space: 12.6 MB (9.52%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -5,11 +5,12 @@ public:
         int m = t.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         function<int(int,int)> go = [&](int n,int m){
-            if(n == 0 && m == 0)
-                return 0;
-            if(n == 0 || m == 0)
-                return n?n:m;
             if(dp[n][m] != -1) return dp[n][m];
+            if(n == 0 && m == 0)
+                return dp[n][m] = 0;
+            if(n == 0 || m == 0)
+                return dp[n][m] = n?n:m;
+            
             int ans = (s[n-1]==t[m-1]?go(n-1,m-1):1+min(go(n-1,m),go(n,m-1)));
             return dp[n][m] = ans;
         };
