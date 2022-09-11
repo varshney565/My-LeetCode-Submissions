9374c4ee6c5383961be18c85f0cc56113ecd5ba9
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-11 11:32:30 +0530

    Time: 960 ms (15.26%), Space: 11.3 MB (19.04%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+2,-1));
-        function<int(int,int)> go = [&](int ind,int prev){
-            if(ind == n)
-                return dp[ind][prev] = 0;
-            
-            if(dp[ind][prev] != -1) return dp[ind][prev];
-            
-            if(prev == n+1 || nums[prev] < nums[ind]){
-                return dp[ind][prev] = max(go(ind+1,prev),1+go(ind+1,ind));
-            }else{
-                return dp[ind][prev] = go(ind+1,prev);
+        vector<int> dp(n,-1);
+        function<int(int)> go = [&](int ei){
+            if(dp[ei] != -1)
+                return dp[ei];
+            int ans = 1;
+            for(int i = ei-1 ; i >= 0 ; i--){
+                if(nums[ei] > nums[i]){
+                    ans = max(ans,1+go(i));
+                }
             }
+            return dp[ei] = ans;
         };
-        return go(0,n+1);
+        int ans = 0;
+        for(int i = n-1 ; i >= 0 ; i--){
+            ans = max(ans,go(i));
+        }
+        return ans;
     }
 };
