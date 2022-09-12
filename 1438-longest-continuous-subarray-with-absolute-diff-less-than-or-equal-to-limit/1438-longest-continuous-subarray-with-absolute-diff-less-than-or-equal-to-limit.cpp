b484d826951171e6d843d35a357e53147d9c0d73
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-09-12 22:38:41 +0530

    Time: 658 ms (5.05%), Space: 65.5 MB (37.51%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,19 +1,94 @@
+class MinSegTree{
+    public:
+    vector<int> arr;
+    vector<int> Seg;
+    int n;
+    MinSegTree(vector<int> &v){
+        arr.insert(arr.end(),v.begin(),v.end());
+        Seg.resize(4*arr.size(),0);
+        n = arr.size();
+    }
+    
+    void build(int high,int low = 0,int ind = 0){
+        if(low == high){
+            Seg[ind] = arr[low];
+            return;
+        }
+        int mid = (low+high)/2;
+        build(mid,low,2*ind+1);
+        build(high,mid+1,2*ind+2);
+        Seg[ind] = min(Seg[2*ind+1],Seg[2*ind+2]);
+    }
+    
+    int query(int l,int r,int low,int high,int ind){
+        if(low > r || l > high){
+            return INT_MAX;
+        }
+        if(low >= l && high <= r){
+            return Seg[ind];
+        }
+        int mid = (low+high)/2;
+        int left = query(l,r,low,mid,2*ind+1);
+        int right = query(l,r,mid+1,high,2*ind+2);
+        return min(left,right);
+    }
+};
+
+class MaxSegTree{
+    public:
+    vector<int> arr;
+    vector<int> Seg;
+    int n;
+    MaxSegTree(vector<int> &v){
+        arr.insert(arr.end(),v.begin(),v.end());
+        Seg.resize(4*arr.size(),0);
+        n = arr.size();
+    }
+    
+    void build(int high,int low = 0,int ind = 0){
+        if(low == high){
+            Seg[ind] = arr[low];
+            return;
+        }
+        int mid = (low+high)/2;
+        build(mid,low,2*ind+1);
+        build(high,mid+1,2*ind+2);
+        Seg[ind] = max(Seg[2*ind+1],Seg[2*ind+2]);
+    }
+    
+    int query(int l,int r,int low,int high,int ind){
+        if(low > r || l > high){
+            return INT_MIN;
+        }
+        if(low >= l && high <= r){
+            return Seg[ind];
+        }
+        int mid = (low+high)/2;
+        int left = query(l,r,low,mid,2*ind+1);
+        int right = query(l,r,mid+1,high,2*ind+2);
+        return max(left,right);
+    }
+};
+
+
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
-        deque<int> Min,Max;
+        MinSegTree S1(nums);
+        MaxSegTree S2(nums);
+        S1.build(n-1);S2.build(n-1);
+        //build(n-1)
+        //query(l,r,0,n-1,0)
         int i = 0,j = 0;
         int ans = 0;
         while(j < n){
-            while(!Min.empty() && nums[Min.back()] > nums[j]) Min.pop_back();
-            while(!Max.empty() && nums[Max.back()] < nums[j]) Max.pop_back();
-            Max.push_back(j);
-            Min.push_back(j);
-            while(nums[Max.front()]-nums[Min.front()] > limit){
-                if(Max.front() == i) Max.pop_front();
-                if(Min.front() == i) Min.pop_front();
+            int min_ = S1.query(i,j,0,n-1,0);
+            int max_ = S2.query(i,j,0,n-1,0);
+            while(max_ - min_ > limit){
                 i++;
+                max_ = S2.query(i,j,0,n-1,0);
+                min_ = S1.query(i,j,0,n-1,0);
             }
             ans = max(ans,j-i+1);
             j++;
