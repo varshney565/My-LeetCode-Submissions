1100e57040160da65c268dea7374366316fb6bc7
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-17 15:32:06 +0530

    Time: 513 ms (45.46%), Space: 104.9 MB (76.54%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -13,6 +13,9 @@ public:
                 ans = min(ans,i+1);
             }
             //neagitive elements can be there at the front of dequeue
+            //that's why we have to remove the elements from the front of the queue irrespective of 
+            //the sum is greater than k or not because there can be a case such that sum is not greater than
+            //k but excluding the front element of the queue making the sum greater than k.
             while(!Pre.empty() && nums[i] - nums[Pre.front()] >= k){
                 ans = min(ans,i-Pre.front());
                 Pre.pop_front();
