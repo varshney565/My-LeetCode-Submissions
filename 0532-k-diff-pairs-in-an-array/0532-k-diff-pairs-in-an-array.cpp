b23a48cc915ff7088488aa2eab394892f811f9fd
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-18 14:47:25 +0530

    Time: 78 ms (10.31%), Space: 13.7 MB (31.71%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -4,20 +4,32 @@ public:
         int n = nums.size();
         int ans = 0;
         map<int,int> cnt;
+        //take each and every element in the set in prior.
         for(int i = 0 ; i < n ; i++){
             cnt[nums[i]]++;
         }
+        
         for(int i = 0 ; i < n ; i++){
+            //if nums[i] is not in the set mean it is already processed.
             if(cnt.find(nums[i]) == cnt.end()) continue;
+            //decreasing the frequency just to avoid the case when k = 0,
+            //because in that case nums[i]-k will lead to the same element
+            //so there must be one more element of the same type at different index,
+            //for holding the condition to be true.
             cnt[nums[i]]--;
             if(cnt[nums[i]] == 0)
                 cnt.erase(nums[i]);
+            //checking whether nums[i]-k is present in the set or not.
             if(cnt.find(nums[i]-k) != cnt.end()){
                 ans += 1;
             }
+            //checking whether nums[i]+k is present in the set or not.
+            //in case k is zero the both nums[i]-k and nums[i]+k will lead to the same element so 
+            //making sure that k is positive for nums[i]+k.
             if(k && cnt.find(nums[i]+k) != cnt.end()){
                 ans += 1;
             }
+            //deleting the nums[i] from the set,because it is processed now.
             cnt.erase(nums[i]);
         }
         return ans;
