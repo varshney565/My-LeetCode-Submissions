d8ef3896f47bfbaa0b9647980135b78379364bef
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-20 15:53:34 +0530

    Time: 12 ms (44.57%), Space: 10.9 MB (76.52%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -5,9 +5,10 @@ public:
         int st = 0;
         int en = 1;
         int cnt = 1;
+        int k = 2;
         for(; en < n ; en++){
-            //element at starting index now has frequency 2. 
-            if(nums[en] == nums[st] && cnt == 2)
+            //element at starting index now has frequency k
+            if(nums[en] == nums[st] && cnt == k)
                 continue;
             //element at starting index is not equal to en means new elements comes
             if(nums[en] != nums[st])
