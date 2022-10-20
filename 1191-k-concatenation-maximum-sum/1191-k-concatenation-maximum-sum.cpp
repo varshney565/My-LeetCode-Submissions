37694f503914e74f750eef5cc5d4e7b2c761c2e1
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-20 21:52:11 +0530

    Time: 158 ms (28.87%), Space: 39.5 MB (63.67%) - LeetHub

--- 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -9,33 +9,31 @@ public:
         b) (part of a1) a2 a3 a4 (part of a5)
         c) maximum circuler subarray sum
         */
-        const int mod = 1e9+7;
         int n = arr.size();
-        int sum = 0;
-        int gMin = int(1e9);
-        int gMax = int(-1e9);
-        int cMinSum = 0;
-        int cMaxSum = 0;
         int gsum = 0;
         for(int i = 0 ; i < n ; i++){
             gsum += arr[i];
         }
+        const int mod = 1e9+7;
+        
+        
+        int sum = 0;
+        int gMax = 0;
+        int cMaxSum = 0;
         int maxSuf = gsum;
         int maxPre = int(-1e9);
         for(int i = 0 ; i < n ; i++){
             sum += arr[i];
             maxSuf = max(maxSuf,gsum-sum);
             maxPre = max(sum,maxPre);
-            cMinSum = min(cMinSum+arr[i],arr[i]);
             cMaxSum = max(cMaxSum+arr[i],arr[i]);
             gMax = max(gMax,cMaxSum);
-            gMin = min(gMin,cMinSum);
         }
         
         if(k == 1) return gMax;
-        int value1 = max(gMax,sum-gMin);        //Maximum subarray sum.
+        long long value1 = gMax;
         long long value2 = (long long)sum*k;
-        long long value3 = (long long)(k-2)*sum+maxSuf+maxPre;
+        long long value3 = max(0ll,(long long)(k-2)*sum)+maxSuf+maxPre;
         long long ans = max(max(value1,value2),value3);
         return ans%mod;
     }
