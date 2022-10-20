a7c1fb6820192ef44e532aaa9981b249544ea4e6
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-20 22:05:26 +0530

    Time: 213 ms (5.36%), Space: 39.5 MB (63.67%) - LeetHub

--- 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -1,27 +1,9 @@
-long long max(long long a,long long b){
-    return (a<b)?b:a;
-}
 class Solution {
 public:
     int kConcatenationMaxSum(vector<int>& arr, int k) {
-        /**
-        a) a1 a2 a3 a4 a5
-        b) (part of a1) a2 a3 a4 (part of a5)
-        c) maximum circuler subarray sum
-        */
-        int n = arr.size();
-        int gsum = 0;
-        for(int i = 0 ; i < n ; i++){
-            gsum += arr[i];
-        }
-        const int mod = 1e9+7;
-        
-        
-        int sum = 0;
-        int gMax = 0;
-        int cMaxSum = 0;
-        int maxSuf = gsum;
-        int maxPre = 0;
+        int n = arr.size(),gsum = 0,sum = 0,gMax = 0,cMaxSum = 0,mod = 1e9+7;
+        gsum = accumulate(arr.begin(),arr.end(),gsum);
+        int maxSuf = gsum,maxPre = 0;
         for(int i = 0 ; i < n ; i++){
             sum += arr[i];
             maxSuf = max(maxSuf,gsum-sum);
@@ -31,7 +13,6 @@ public:
         }
         
         if(k == 1) return gMax;
-        long long ans = (max(max(0ll,(long long)(k-2)*sum)+maxSuf+maxPre,gMax))%mod;
-        return ans;
+        return (max(max(0ll,(long long)(k-2)*sum)+maxSuf+maxPre,(long long)gMax))%mod;
     }
 };
