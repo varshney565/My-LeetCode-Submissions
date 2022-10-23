387c5adfefb3646efda38dc3a329afc0a4b2b8a9
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-23 19:25:40 +0530

    Time: 1378 ms (5.02%), Space: 135.8 MB (6.23%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
+        // int n = nums.size();
+        // deque<pair<int,int>> d;
+        // int i = 0;
+        // while(i < n){
+        //     //erase all the elements that needed to be deleted.
+        //     while(!d.empty() && d.front().first < i-k)
+        //         d.pop_front();
+        //     //do the processing.
+        //     int max_ = (d.empty()?0:d.front().second)+nums[i];
+        //     //insert all the needed elements.
+        //     while(!d.empty() && d.back().second < max_)
+        //         d.pop_back();
+        //     d.push_back({i,max_});
+        //     i++;
+        // }
+        // return d.back().second;
+        
+        
+        int minJump = 1;
+        int maxJump = k;
         int n = nums.size();
+        int i = minJump;
+        map<int,int> processedElements;
         deque<pair<int,int>> d;
-        int i = 0;
+        processedElements.insert({0,nums[0]});
         while(i < n){
-            //erase all the elements that needed to be deleted.
-            while(!d.empty() && d.front().first < i-k)
+            //insert the needy elements
+            int eleInd = i-minJump;
+            if(processedElements.find(eleInd) != processedElements.end()){
+                while(!d.empty() && d.back().second < processedElements[eleInd])
+                    d.pop_back();
+                d.push_back({eleInd,processedElements[eleInd]});
+            }
+            //remove the element 
+            while(!d.empty() && d.front().first < i-maxJump)
                 d.pop_front();
-            //do the processing.
+            //Actutal processing 
             int max_ = (d.empty()?0:d.front().second)+nums[i];
-            //insert all the needed elements.
-            while(!d.empty() && d.back().second < max_)
-                d.pop_back();
-            d.push_back({i,max_});
+            processedElements.insert({i,max_});
             i++;
         }
-        return d.back().second;
+        return (processedElements.find(n-1) != processedElements.end())?processedElements[n-1]:-1;
     }
 };
