b6799d69acc6d5a69b7710d100e188bf02e9d8e2
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-25 13:44:32 +0530

    Time: 88 ms (85.25%), Space: 17 MB (87.08%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -9,18 +9,25 @@ public:
         map<string,int> included;
         int countInitial = count;
         while(en < n){
+            //current word that needed to be processed.
             string temp = s.substr(en,wl);
+            //if all the required words are not processed yet means count > 0
             if(count > 0){
+                //it that word needs to be present in the final window.
                 if(words.find(temp) != words.end()){
+                    //if the all such words are present in the final window.
                     while(words[temp] <= included[temp]){
                         string del = s.substr(st,wl);
                         included[del]--;
                         st += wl;
                         count++;
                     }
+                    //include that word.
                     included[temp]++;
                     count--;
                 }else{
+                    //it that word need not to be present in the final window so whole window 
+                    //should be reevaluated because we found a word that need not to be present.
                     included.clear();
                     en += wl;
                     st = en;
@@ -29,6 +36,8 @@ public:
                 }
             }
             
+            //if we found a window with all such words that we will update the answer and 
+            //shift the window.
             if(count == 0){
                 ans.push_back(st);
                 string del = s.substr(st,wl);
@@ -36,6 +45,7 @@ public:
                 st += wl;
                 count++;
             }
+            //pass the control to the next word.
             en += wl;
         }
         return ans;
@@ -51,7 +61,6 @@ public:
         map<string,int> word;
         for(auto x : words)
             word[x]++;
-        
         
         for(int i = 0 ; i < wl ; i++){
             auto t = go(i,word,s,w);
