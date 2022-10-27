40d126eeefdbb7987e8afb605bbb5a80df7ef6a1
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-10-27 18:28:43 +0530

    Time: 16 ms (90.15%), Space: 13.4 MB (20.74%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,11 +5,15 @@ public:
         int m = matrix[0].size();
         vector<int> row(n,false);
         vector<bool> col(m,false);
+        //if mat[i][j] = 0 set all the values in row i and col j as 0
+        
+        //find all such rows and columns such that they needed to be set as 0
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(!matrix[i][j]) row[i] = true,col[j] = true;
             }
         }
+        //setting rows and columns as 0.
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(row[i] || col[j]) matrix[i][j] = 0;
