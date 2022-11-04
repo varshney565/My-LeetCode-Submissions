c5a2e82936cfe21fa5568cddc6e84fee0731da5c
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-11-04 17:41:42 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (51.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,37 +1,13 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        /**
-        What Question demands ??
-        find the lower_bound of target_row assumng the first column to be sorted in decreasing order.
-        **/
-        
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int s = 0;
-        int e = n-1;
+        int n = matrix.size(),m = matrix[0].size(),s = 0,e = n*m-1;
         while(s <= e){
             int mid = s+(e-s)/2;
-            if(matrix[mid][0] > target){
-                e = mid-1;
-            }else{
+            if(matrix[mid/m][mid%m] < target){
                 s = mid+1;
-            }
-        }
-        //now e will have the access to the element that we needed.
-        //because [row no : e + 1] will gonna start from [target + k,...........so on]
-        int r = e;
-        if(r < 0) return false;
-        s = 0;
-        e = m-1;
-        //now search in row for the target.
-        //if found return true else return false.
-        while(s <= e){
-            int mid = s+(e-s)/2;
-            if(matrix[r][mid] > target){
+            }else if(matrix[mid/m][mid%m] > target){
                 e = mid-1;
-            }else if(matrix[r][mid] < target){
-                s = mid+1;
             }else{
                 return true;
             }
