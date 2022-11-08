182689bf8f8bd1af21e41f78743e1de2b428d850
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-11-08 20:50:27 +0530

    Time: 161 ms (58.70%), Space: 24.9 MB (19.39%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        int i = 0;
-        while(i < n-2){
-            int si = i+1,ei = n-1;
-            while(si < ei){
-                if(nums[i]+nums[si]+nums[ei] > 0) ei--;
-                else if(nums[i]+nums[si]+nums[ei] < 0) si++;
-                else{
-                    ans.push_back({nums[i],nums[si],nums[ei]});
-                    si++;
-                    ei--;
-                    while(si < ei && nums[si] == nums[si-1]) si++;
-                    while(si < ei && nums[ei] == nums[ei+1]) ei--;
-                }
+    vector<vector<int>> k_sum(vector<int>&nums,long long target,int si,int ei,int k){
+        
+        if(k == 2){
+            int i = si,j = ei;
+            vector<vector<int>> ans;
+            while(i < j){
+                if(nums[i]+nums[j] == target){
+                    ans.push_back({nums[i],nums[j]});
+                    i++;
+                    j--;
+                    while(i < j && nums[i] == nums[i-1]) i++;
+                    while(i < j && nums[j] == nums[j+1]) j--;
+                }else if(nums[i]+nums[j] > target) j--;
+                else i++;
             }
-            i++;
-            while(i < n-2 && nums[i] == nums[i-1])
-                i++;
+            return ans;
         }
+        
+        vector<vector<int>> ans;
+        for(int i = ei ; i >= si ; ){
+            vector<vector<int>> SmallAns = k_sum(nums,target-nums[i],si,i-1,k-1);
+            for(auto &x : SmallAns){
+                x.push_back(nums[i]);
+                ans.push_back(x);
+            }
+            i--;
+            while(i >= si && nums[i] == nums[i+1]) i--;
+        }
+        return ans;
+    }
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans = k_sum(nums,0ll,0,nums.size()-1,3);
         return ans;
     }
 };
