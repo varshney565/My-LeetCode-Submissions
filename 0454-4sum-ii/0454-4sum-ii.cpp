371b13f677ba26f94aaef06ed8ab9f5d6e6ebd03
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-11-08 21:27:20 +0530

    Time: 721 ms (9.55%), Space: 24.6 MB (35.04%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        map<int,int> Container;
+        
+        for(int i = 0 ; i < nums2.size() ; i++){
+            for(int j = 0 ; j < nums1.size() ; j++){
+                Container[nums1[j]+nums2[i]]++;
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0 ; i < nums3.size() ; i++){
+            for(int j = 0 ; j < nums4.size() ; j++){
+                int s = nums3[i]+nums4[j];
+                if(Container.find(-s) != Container.end())
+                    ans += Container[-s];
+            }
+        }
+        return ans;
+    }
+};
