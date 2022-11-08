128a43595e4ff14c13c6938fe4017f97556c1edc
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-11-08 20:47:51 +0530

    Time: 93 ms (74.54%), Space: 13.6 MB (34.66%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        for(int i = 0 ; i < n-3 ; ){
-            for(int j = i + 1 ; j < n-2 ; ){
-                int s = j+1,e = n-1;
-                while(s < e){
-                    long long sum = (long long)nums[i]+nums[j]+nums[s]+nums[e];
-                    if(sum > target) e--;
-                    else if(sum < target) s++;
-                    else{
-                        ans.push_back({nums[i],nums[j],nums[s],nums[e]});
-                        s++;
-                        e--;
-                        while(s < e && nums[s] == nums[s-1]) s++;
-                        while(s < e && nums[e] == nums[e+1]) e--;
-                    }
-                }
-                j++;
-                while(j < n-2 && nums[j] == nums[j-1]) j++;
+    vector<vector<int>> k_sum(vector<int>&nums,long long target,int si,int ei,int k){
+        if(k == 2){
+            int i = si,j = ei;
+            vector<vector<int>> ans;
+            while(i < j){
+                if(nums[i]+nums[j] == target){
+                    ans.push_back({nums[i],nums[j]});
+                    i++;
+                    j--;
+                    while(i < j && nums[i] == nums[i-1]) i++;
+                    while(i < j && nums[j] == nums[j+1]) j--;
+                }else if(nums[i]+nums[j] > target) j--;
+                else i++;
             }
-            i++;
-            while(i < n-3 && nums[i] == nums[i-1]) i++;
+            return ans;
         }
+        
+        vector<vector<int>> ans;
+        for(int i = ei ; i >= si ; ){
+            vector<vector<int>> SmallAns = k_sum(nums,target-nums[i],si,i-1,k-1);
+            for(auto &x : SmallAns){
+                x.push_back(nums[i]);
+                ans.push_back(x);
+            }
+            i--;
+            while(i >= si && nums[i] == nums[i+1]) i--;
+        }
+        return ans;
+    }
+    
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans = k_sum(nums,(long long)target,0,nums.size()-1,4);
         return ans;
     }
 };
