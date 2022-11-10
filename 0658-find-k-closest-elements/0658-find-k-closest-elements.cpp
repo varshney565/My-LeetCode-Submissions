cc2bd698081a537b83649b8dc9997a1c5c8c768f
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-11-10 20:32:23 +0530

    Time: 43 ms (96.44%), Space: 31.1 MB (83.19%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,38 +2,16 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
-        vector<int> ans;
-        // find the upper bound and lower bound of x and start from there.
-        sort(arr.begin(),arr.end());
-        int s = 0,e = n-1;
-        while(s <= e){
+        int s = 0;
+        int e = n-k;
+        while(s < e){
             int mid = (s+e)/2;
-            if(arr[mid] > x) e = mid-1;
-            else s = mid+1;
-        }
-        //now s is pointing to upper_bound of x in arr.
-        while(s < n && e >= 0 && ans.size() < k){
-            int val1 = abs(arr[s]-x);
-            int val2 = abs(arr[e]-x);
-            if(val1 < val2){
-                ans.push_back(arr[s]);
-                s++;
-            }else if(val2 <= val1){
-                ans.push_back(arr[e]);
-                e--;
+            if(x-arr[mid] > arr[mid+k]-x){
+                s = mid+1;
+            }else{
+                e = mid;
             }
         }
-        
-        while(s < n && ans.size() < k){
-            ans.push_back(arr[s]);
-            s++;
-        }
-        
-        while(e >= 0 && ans.size() < k){
-            ans.push_back(arr[e]);
-            e--;
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+        return vector<int>(arr.begin()+s,arr.begin()+s+k);
     }
 };
