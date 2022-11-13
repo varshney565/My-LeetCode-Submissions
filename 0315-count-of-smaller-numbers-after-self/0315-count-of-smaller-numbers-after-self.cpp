42f1d91c19bd7a4493928533c8b4929a8fd86fae
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2022-11-13 14:33:36 +0530

    Time: 2182 ms (11.95%), Space: 301 MB (17.66%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,23 +1,54 @@
-/**
-    Using policy based data structure.
-*/
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-
-#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
-
 class Solution {
 public:
-    vector<int> countSmaller(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n,0);
-        ordered_set store;
-        for(int i = n-1 ; i >= 0 ; i--){
-            ans[i] = store.order_of_key(nums[i]);
-            store.insert(nums[i]);
+    
+    void merge(vector<int> &nums,vector<int> &idx,vector<int> &ans,int si,int mid,int ei){
+        vector<int> res(ei-si+1);
+        vector<int> res2(ei-si+1);
+        int i = si;
+        int j = mid+1;
+        int k = 0;
+        while(i <= mid && j <= ei){
+            if(nums[i] > nums[j]){
+                res[k] = nums[j];
+                res2[k++] = idx[j++];
+            }else{
+                ans[idx[i]] += j-mid-1;
+                res[k] = nums[i];
+                res2[k++] = idx[i++];
+            }
         }
+        
+        while(i <= mid){
+            ans[idx[i]] += j-mid-1;
+            res[k] = nums[i];
+            res2[k++] = idx[i++];
+        }
+        
+        while(j <= ei){
+            res[k] = nums[j];
+            res2[k++] = idx[j++];
+        }
+        
+        for(int i = si ; i <= ei ; i++){
+            nums[i] = res[i-si];
+            idx[i] = res2[i-si];
+        }
+    }
+    
+    void merge_sort(vector<int> &nums,vector<int> &idx,vector<int> &ans,int si,int ei){
+        if(si >= ei) return;
+        
+        int mid = si+(ei-si)/2;
+        merge_sort(nums,idx,ans,si,mid);
+        merge_sort(nums,idx,ans,mid+1,ei);
+        merge(nums,idx,ans,si,mid,ei);
+    }
+    
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> ans(nums.size(),0);
+        vector<int> idx(nums.size(),0);
+        for(int i = 0 ; i < nums.size() ; i++)  idx[i] = i;
+        merge_sort(nums,idx,ans,0,nums.size()-1);
         return ans;
     }
 };
