c0fa3d3880ade91a5dda93f35ed0d97bee806973
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-04 15:14:51 +0530

    Time: 28 ms (96.11%), Space: 11.5 MB (89.90%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -5,9 +5,17 @@ public:
         int ans = n;
         int s01 = 0,s10 = 0;
         for(int i = 0 ; i < n ; i++){
-            if(i%2 == (s[i]-'0'))s10++; 
+            if(i%2 == (s[i]-'0')) s10++; 
             else s01++;
         }
+        /**
+          * c01 and c10 is the cost to make string[0,i] in 0101 and 1010 order respectively.
+          **/
+        
+        /**
+          * s01 and s10 is the cost to make string[i+1,n-1] in 0101 and 1010 order respectively.
+          **/
+        
         int c01 = 0,c10 = 0;
         for(int i = 0 ; i < n ; i++){
             if((n-i)%2 == 0) ans = min(ans,min(s10+c10,s01+c01));
