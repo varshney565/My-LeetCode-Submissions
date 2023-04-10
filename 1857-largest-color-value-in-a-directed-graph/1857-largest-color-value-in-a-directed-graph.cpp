dab50dff0b974119d1dcbea4b2cc34676915de1b
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-10 16:01:32 +0530

    Time: 659 ms (52.59%), Space: 226.6 MB (13.36%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n = colors.size(),k=26;
-        vector<int> indegrees(n, 0);
-        vector<vector<int>> graph(n, vector<int>());
-        for (auto edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            indegrees[v]++;
+        int n = colors.size();
+        vector<vector<int>> graph(n);
+        vector<int> indeg(n,0);
+        for(int i = 0 ; i < edges.size() ; i++){
+            graph[edges[i][0]].push_back(edges[i][1]);
+            indeg[edges[i][1]]++;
         }
-        unordered_set<int> zero_indegree;
-        for (int i = 0; i < n; i++) {
-            if (indegrees[i] == 0) {
-                zero_indegree.insert(i);
-            }
-        }
-        vector<vector<int>> counts(n, vector<int>(k, 0));
-        for (int i = 0; i < n; i++) {
-            counts[i][colors[i] - 'a']++;
-        }
-        int max_count = 0,visited = 0;
-        while (!zero_indegree.empty()) {
-            int u = *zero_indegree.begin();
-            zero_indegree.erase(u);
-            visited++;
-            for (int v : graph[u]) {
-                for (int i = 0; i < k; i++)
-                    counts[v][i] = max(counts[v][i], counts[u][i] + (colors[v] - 'a' == i ? 1 : 0));
-                indegrees[v]--;
-                if (indegrees[v] == 0) {
-                    zero_indegree.insert(v);
+        
+        vector<int> visited(n,0);
+        vector<vector<int>> dp(n,vector<int>(26,0));
+        function<bool(int)> dfs = [&](int src){
+            visited[src] = 1;
+            for(auto child : graph[src]){
+                if(visited[child] == 0){
+                    bool ans = dfs(child);
+                    if(!ans) return ans;
+                }else if(visited[child] == 1){
+                    return false;
+                }
+                for(int j = 0 ; j < 26 ; j++){
+                    dp[src][j] = max(dp[src][j],dp[child][j]);
                 }
             }
-            max_count = max(max_count, *max_element(counts[u].begin(), counts[u].end()));
+            dp[src][colors[src]-'a']++;
+            visited[src] = 2;
+            return true;
+        };
+        
+        for(int i = 0 ; i < n ; i++){
+            if(indeg[i] == 0){
+                bool res = dfs(i);
+                if(!res) return -1;
+            }
         }
-        return visited == n ? max_count : -1;
+
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(!visited[i]) return -1;
+            if(indeg[i] == 0){
+                for(int j = 0 ; j < 26 ; j++){
+                    ans = max(ans,dp[i][j]);
+                }
+            }
+        }
+        return ans;
     }
 };
