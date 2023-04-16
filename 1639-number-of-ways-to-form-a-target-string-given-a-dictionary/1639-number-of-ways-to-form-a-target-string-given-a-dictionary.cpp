cd07af55711c9a76ee59421f829eb8e02ab82756
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-16 22:35:36 +0530

    Time: 210 ms (84.48%), Space: 54.8 MB (42.24%) - LeetHub

---
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    const int mod = 1e9+7;
+    int numWays(vector<string>& words, string target) {
+        int n1 = target.size();
+        int n2 = words[0].size();
+        int m = words.size();
+        vector<vector<int>> cnt(n2,vector<int>(26,0));
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n2 ; j++){
+                cnt[j][words[i][j]-'a']++;
+            }
+        }
+        
+        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        function<int(int,int)> go = [&](int n,int m){
+            if(m < n) return 0;
+            if(n < 0) return 1;
+            if(dp[n][m] != -1) return dp[n][m];
+            //option1 :: take that char count.
+            int ans = 0;
+            int val = cnt[m][target[n]-'a'];
+            if(val) {
+                long long t = go(n-1,m-1);
+                ans = (ans + (val*t)%mod)%mod;
+            }
+            //option2 :: don't take that char count.
+            ans = (ans + go(n,m-1))%mod;
+            return dp[n][m] = ans;
+        };
+        return go(n1-1,n2-1);
+    }
+};
