a0a47a8232abaf54d6161874afdc2afc71166d12
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-20 15:26:25 +0530

    Time: 49 ms (11.77%), Space: 12.6 MB (95.20%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& Edges, int src, int dst, int k) {
-        /**
-        what is asked in the questin ??
-        
-        find the minimum cost to reach from src to dest with using atmost k stops and with minimum cost
-        
-        here there are two constrains : 
-        
-        a ) find the minimum distance.
-        b ) using atmost k stops.
-        
-        but we can apply dj only if there is only one constrain.
-        
-        so apply bellman ford
-        */
-        
         k++;
         vector<int> d(n,INT_MAX);
         d[src] = 0;
