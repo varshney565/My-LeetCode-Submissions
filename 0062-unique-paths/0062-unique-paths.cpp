9f50e86683b23ce44e9863a2040c585aeffc6437
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-24 19:16:28 +0530

    Time: 2 ms (44.52%), Space: 6.6 MB (33.95%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dir[][2] = {{-1,0},{0,-1}};
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        function<int(int,int)> go = [&](int x,int y){
+            if(x == 0 && y == 0) return dp[x][y] = 1;
+            if(dp[x][y] != -1) return dp[x][y];
+            int ans = 0;
+            for(int i = 0 ; i < 2 ; i++){
+                int newx = x+dir[i][0];
+                int newy = y+dir[i][1];
+                if(newx >= 0 && newy >= 0){
+                    ans += go(newx,newy);
+                }
+            }
+            return dp[x][y] = ans;
+        };
+        return go(m-1,n-1);
+    }
+};
