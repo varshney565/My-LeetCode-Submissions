f0daa943faa78397da9d771b178c21f0b9280b50
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-25 14:49:51 +0530

    Time: 3 ms (74.33%), Space: 7.9 MB (15.30%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,7 +4,7 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         int dir[][2] = {{-1,0},{0,-1}};
-        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        
         vector<vector<int>> dp(m,vector<int>(n,-1));
         function<int(int,int)> go = [&](int x,int y){
             if(obstacleGrid[x][y] == 1) return dp[x][y] = 0;
