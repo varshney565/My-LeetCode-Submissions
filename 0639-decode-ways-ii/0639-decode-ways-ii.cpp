10e2c824cbdfa5117495dd126348d7a2ded87325
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-25 22:52:57 +0530

    Time: 64 ms (36.63%), Space: 44.9 MB (17.49%) - LeetHub

---
+++ 0639-decode-ways-ii/0639-decode-ways-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        const int mod = 1e9+7;
+        vector<int> dp(n,-1);
+        function<int(int)> go = [&](int i){
+            if(i < 0) return 1;
+            if(dp[i] != -1) return dp[i];
+            //consider only ith character
+            int ans = 0;
+            if(s[i] != '0') ans = ((long long)go(i-1)*(s[i] == '*' ? 9ll : 1ll))%mod;
+            
+            //consider both ith and (i-1)th character
+            if(i){
+                if(s[i-1] != '0'){
+                    long long f = 1;
+                    if(s[i] == '*' && s[i-1] == '*'){
+                        f = 15;
+                    }else if(s[i] == '*'){
+                        f = (s[i-1] == '1') ? 9 : (s[i-1] == '2' ? 6 : 0);
+                    }else if(s[i-1] == '*'){
+                        f = (s[i] >= '0' && s[i] <= '6') ? 2 : 1;
+                    }else{
+                        int num = (s[i-1]-'0')*10 + (s[i]-'0');
+                        if(!(num >= 1 && num <= 26))
+                            f = 0;
+                    }
+                    ans = (ans + (go(i-2)*f)%mod)%mod;
+                }
+            }
+            return dp[i] = ans;
+        };
+        return go(n-1);
+    }
+};
