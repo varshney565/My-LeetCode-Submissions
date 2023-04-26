71db3d48428ff7de34e96292ce4c71cdf305c596
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-04-26 09:57:00 +0530

    Time: 30 ms (92.68%), Space: 11.8 MB (91.40%) - LeetHub

--- 0639-decode-ways-ii/0639-decode-ways-ii.cpp
+++ 0639-decode-ways-ii/0639-decode-ways-ii.cpp
@@ -6,9 +6,7 @@ public:
         long long a = 1,b = 1;
         for(int i = 0 ; i < n ; i++){
             int c = 0;
-            //first
             if(s[i] != '0') c = (b*(s[i] == '*' ? 9 : 1))%mod;
-            //second
             if(i && s[i-1] != '0'){
                 long long f = 1,num = (s[i-1]-'0')*10+(s[i]-'0');
                 if(s[i] == '*' && s[i-1] == '*') f = 15;
