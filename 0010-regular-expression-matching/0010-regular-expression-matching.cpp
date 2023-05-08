c823e670e692f7d3a0e4d35b8cd4b7f6b196d536
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-05-08 15:49:26 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (22.06%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -5,18 +5,14 @@ public:
         int m = p.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         function<int(int,int)> go = [&](int n,int m){
-            if(m == 0 && n == 0){
-                return dp[n][m] = 1;
-            }
+            if(m == 0 && n == 0) return dp[n][m] = 1;
             if(n == 0){
                 for(int i = m - 1 ; i >= 0 ; i-=2){
                     if(p[i] != '*') return dp[n][m] = 0;
                 }
                 return dp[n][m] = 1;
             }
-            if(m == 0){
-                return dp[n][m] = 0;
-            }
+            if(m == 0) return dp[n][m] = 0;
             if(dp[n][m] != -1) return dp[n][m];
             bool ans = false;
             if(p[m-1] == '.' || s[n-1] == p[m-1]){
