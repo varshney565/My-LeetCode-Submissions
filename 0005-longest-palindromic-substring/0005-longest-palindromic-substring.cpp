f0de3daba7945ecfbb16f22f04800eab85f19c38
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-05-23 11:58:59 +0530

    Time: 664 ms (23.59%), Space: 22.6 MB (36.07%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    void manacher(string &p,vector<int> &t){
-        int C = 0,R = 0;
-        int n = p.size();
-        for(int i = 1 ; i < n - 1 ; i++){
-            int mirror = 2*C-i;
-            if(i < R){
-                t[i] = min(t[mirror],R-i);
-            }
-            while(p[i+(1+t[i])] == p[i-(1+t[i])])
-                t[i]++;
-            if(i+t[i] > R){
-                C = i;
-                R = 1+t[i];
-            }
-        }
-    }
-    
     string longestPalindrome(string s) {
-        string p = "";
-        p += '@';
-        for(auto x : s){
-            p += '#';
-            p += x;
-        }
-        p += '#';
-        p += '$';
-        vector<int> t(p.size(),0);
-        manacher(p,t);
-        int l = 0;
-        for(int i = 0 ; i < p.size() ; i++) l = max(l,t[i]);
-        string ans = "";
-        for(int i = 0 ; i < p.size() ; i++) {
-            if(t[i] == l){
-                for(int j = i-l ; j <= i+l ; j++){
-                    if(p[j] != '#') ans += p[j];
+        int n = s.size();
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        int st = 0;
+        int en = 0;
+        for(int gap = 1 ; gap <= n ; gap++){
+            int i = 0;
+            int j = i+gap-1;
+            while(j < n){
+                if(gap == 1)
+                    dp[i][j] = true;
+                else if(gap == 2){
+                    if(s[i] == s[j]) dp[i][j] = true;
+                }else{
+                    dp[i][j] = ((s[i] == s[j]) ? dp[i+1][j-1] : false);
                 }
-                return ans;
+                if(dp[i][j] && j-i+1 > en-st+1){
+                    st = i;
+                    en = j;
+                }
+                i++;
+                j++;
             }
         }
-        return ans;
+        return s.substr(st,en-st+1);
     }
 };
