392c62f97f338bc40fe2a8acd975f50a08a248ec
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-05-28 13:08:22 +0530

    Time: 18 ms (43.43%), Space: 11.5 MB (52.67%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        set<string> Dict;
+        for(auto x : wordDict) Dict.insert(x);
+        vector<int> dp(n+1,-1);
+        function<int(int)> go = [&](int n){
+            if(n == 0) return dp[n] = 1;
+            if(dp[n] != -1) return dp[n];
+            string word = "";
+            bool ans = false;
+            for(int i = n - 1 ; i >= 0 ; i--){
+                word = s[i] + word;
+                if(Dict.find(word) != Dict.end()){
+                    ans = ans || go(i);
+                }
+            }
+            return dp[n] = (ans?1:0);
+        };
+        return go(n);
+    }
+};
