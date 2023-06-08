5bd16c106861ef08a4f012a3491ca17b129caa57
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-06-08 13:48:48 +0530

    Time: 239 ms (75.19%), Space: 95.9 MB (18.50%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -10,9 +10,7 @@ public:
             if(sum == 0) return dp[n][sum] = 1;
             if(n == 0) return dp[n][sum] = 0;
             if(dp[n][sum] != -1) return dp[n][sum];
-            //not pick
             if(go(n-1,sum)) return dp[n][sum] = 1;
-            //pick
             if(sum - nums[n-1] >= 0)
                 return dp[n][sum] = go(n-1,sum-nums[n-1]);
             return dp[n][sum] = 0;
