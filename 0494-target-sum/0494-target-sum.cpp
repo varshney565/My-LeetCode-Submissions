61d48393e9cfd735d8e287f4817d2fe26da7d925
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-06-08 13:36:25 +0530

    Time: 27 ms (52.97%), Space: 22.7 MB (22.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int origin;
     int go(int n,int target,vector<int>&nums){
         if(n == 0){
-            return (target == 0) ? 1 : 0;
+            return dp[n][target] = (target == origin) ? 1 : 0;
         }
+        if(dp[n][target] != -1) return dp[n][target];
         int count = 0;
         //+
         count = go(n-1,target+nums[n-1],nums);
         //-
         count = count + go(n-1,target-nums[n-1],nums);
-        return count;
+        return dp[n][target] = count;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -17,7 +20,8 @@ public:
         int sum = 0;
         sum = accumulate(nums.begin(),nums.end(),sum);
         if(target > sum || target < -sum) return 0;
-        
-        return go(n,target,nums);
+        dp.resize(n+1,vector<int>(2*sum+1,-1));
+        origin = sum-target;
+        return go(n,target+origin,nums);
     }
 };
