bb0ca7b2179d749bb7b027ec7f9f81e2cf5524f8
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-06-09 13:37:00 +0530

    Time: 21 ms (55.17%), Space: 6.5 MB (97.97%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    double knightProbability(int n, int k, int row, int column) {
+    double knightProbability(int n, int K, int row, int column) {
         int dir[][2] = {{2,1},{2,-1},{-2,1},{-2,-1},{1,2},{-1,2},{1,-2},{-1,-2}};
-        double dp[n+1][n+1][k+1];
+        double dp[n+1][n+1][K+1];
         memset(dp,0.0,sizeof(dp));
-        function<double(int,int,int)> go = [&](int x,int y,int k){
-            if(k == 0) return dp[x][y][k] = 1.0;
-            if(dp[x][y][k] != 0.0) return dp[x][y][k];
-            for(int i = 0 ; i < 8 ; i++){
-                int newx = x+dir[i][0];
-                int newy = y+dir[i][1];
-                if(newx >= 0 && newx < n && newy >= 0 && newy < n){
-                    dp[x][y][k] += go(newx,newy,k-1);
+        
+        for(int k = 0 ; k <= K ; k++){
+            for(int x = 0 ; x < n ; x++){
+                for(int y = 0 ; y < n ; y++){
+                    if(k == 0) {
+                        dp[x][y][k] = 1.0;
+                        continue;
+                    }
+                    for(int i = 0 ; i < 8 ; i++){
+                        int newx = x+dir[i][0];
+                        int newy = y+dir[i][1];
+                        if(newx >= 0 && newx < n && newy >= 0 && newy < n){
+                            dp[x][y][k] += dp[newx][newy][k-1];
+                        }
+                    }
+                    dp[x][y][k] /= 8.0;
                 }
             }
-            return dp[x][y][k] /= 8.0;
-        };
-        return go(row,column,k);
+        }
+        return dp[row][column][K];
     }
 };
