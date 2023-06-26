1d4a65299486a4f7d0a70f34ebd807679cabe1dc
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2023-06-26 22:49:22 +0530

    Time: 12 ms (67.45%), Space: 7.7 MB (65.83%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int t2 = 0, t3 = 0, t5 = 0;
+        vector<int> k(n);
+        k[0] = 1;
+        for(int i = 1; i < n ; i++){
+            k[i] = min(k[t2]*2,min(k[t3]*3,k[t5]*5));
+            if(k[i] == k[t2]*2) t2++; 
+            if(k[i] == k[t3]*3) t3++;
+            if(k[i] == k[t5]*5) t5++;
+        }
+        return k[n-1];
+    }
+};
