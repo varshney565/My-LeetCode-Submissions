2853bee60700d246f9cf23a4d1fc34c56f1a9845
Shivam Varshney <87566789+varshney565@users.noreply.github.com>
2026-07-09 14:05:17 +0530

    Time: 2055 ms (5.04%), Space: 480.5 MB (5.19%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -6,7 +6,6 @@ public:
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
         vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
         vector<vector<int>> distance(n,vector<int>(m,INT_MAX));
-        //wsf,x,y
         pq.push({0,0,0});
         distance[0][0] = 0;
         while(!pq.empty()) {
@@ -15,7 +14,6 @@ public:
             int cost_so_far = top[0];
             int x = top[1];
             int y = top[2];
-            // cout<<x<<" : "<<y<<"\n";
             if(distance[x][y] < cost_so_far) continue;
             if(x == n - 1 && y == m - 1) {
                 return cost_so_far;
